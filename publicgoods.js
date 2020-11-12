25ab2244d5baa2d560f602e01e0dacb152ef2b6f
JosephSCha <josephshincha@gmail.com>
2020-11-11 22:08:22 -0500

    mobile layout

--- publicgoods.js
+++ publicgoods.js
@@ -2,12 +2,25 @@ window.addEventListener('DOMContentLoaded', (event) => {
     const shopBtn = document.getElementById('shop-btn')
     const trialBtn = document.getElementById('trial-btn')
     const homepage = document.getElementById('homepage')
-    const showpage = document.getElementById('bamboo')
-    const nextBtn = document.getElementById('next-btn')
-    const backBtn = document.getElementById('back-btn')
+    const showpage = document.getElementById('showpage')
+    // const nextBtn = document.getElementById('next-btn')
 
-    const pages = ["bamboo", "plastics", "designed", "collection"]
-    let counter = 0
+    // const pages = ["bamboo", "plastics", "designed", "collection"]
+    // let counter = 0
+    
+    // nextPage = () => {
+    //     event.preventDefault();
+    //     currentPage = document.getElementById(pages[counter])
+    //     nextPage = document.getElementById(pages[counter+1])
+    //     currentPage.style.display = 'none'
+    //     nextPage.style.display = 'block'
+    //     if(counter < 4){
+    //         counter += 1
+    //     }
+    //     else {
+    //         counter = 0
+    //     }
+    // }
 
     shopBtn.addEventListener('click', function (event){
         event.preventDefault();
@@ -21,20 +34,14 @@ window.addEventListener('DOMContentLoaded', (event) => {
         showpage.style.display = 'block'
     })
 
-    nextBtn.addEventListener('click', function (event){
-        event.preventDefault();
-        currentPage = document.getElementById(pages[counter])
-        nextPage = document.getElementById(pages[counter+1])
-        currentPage.style.display = 'none'
-        nextPage.style.display = 'block'
-        if(counter < 4){
-            counter += 1
-        }
-        else {
-            counter = 0
-        }
-        console.log(counter)
-    })
+    // nextBtn.addEventListener('click', function (event){
+    //     event.preventDefault();
+    //     currentPage = document.getElementById(pages[counter])
+    //     nextPage = document.getElementById(pages[counter+1])
+    //     currentPage.style.display = 'none'
+    //     nextPage.style.display = 'block'
+    //     addCount()
+    // })
 
 
 });
